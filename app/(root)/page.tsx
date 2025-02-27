65cb0f745bb244b5778bbfab3c061823d8b0e41e
mlaskowski7 <mtlaskowski7@gmail.com>
2025-02-27 12:27:55 +0100

    feat: initial homepage

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="w-full h-full bg-black justify-center items-center">
+      <h1 className="text-blue-700 text font-bold text-2xl text-center mt-5">
+        Mana Manager
+      </h1>
+    </div>
+  );
+};
+
+export default Home;
